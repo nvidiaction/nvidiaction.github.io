40445bb97c3e2a74e7a8d5e32ed0d47f5d220cc0
blog-admin <admin@local>
2026-07-18 06:02:33 +0800

    chore: remove demo posts, set user avatar

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "nvidiaction",
 	bio: "永远相信美好的事情即将发生",
 	links: [
